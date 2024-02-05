e8aeceec519174b63df378c19c0f0d635bf62bf5
PK303-3 <PK303-3@gmail.com>
2024-02-05 12:52:58 +0300

    hgfhgfhgf

---
+++ newproject/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Блог FreeTravel</title>
+    <link rel="icon" href="images/logo.png">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+   <header>
+    <img width="100px" height="100px" src="images/logo.png" alt="Логотип">
+    <h1>Блог FreeTravel</h1>
+    </header>
+    <nav>
+        <a href="/">Главная</a>
+        <a href="/admpanel">Административная панель</a>
+    </nav>
+    <main>
+        <p class="error"> </p>
+        <form>
+            <label>
+                Логин
+                <input type="text" name="login">
+           </label>
+           <label>
+                Пароль
+                <input type="password" name="password">
+           </label>
+           <button>Войти</button>
+        </form>
+    <main>
+</body>
+</html>
